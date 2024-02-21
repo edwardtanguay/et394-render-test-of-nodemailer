8148bb4a953e052a0f4b5c51bc2140164d5e4bbb
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-21 11:24:05 +0100

    can send locally

--- src/routers/employeeRouter.ts
+++ src/routers/employeeRouter.ts
@@ -1,9 +1,46 @@
-import express, { Router } from 'express';
-import * as employeeHandlers from '../handlers/employeeHanders';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import express, { Router } from "express";
+import * as employeeHandlers from "../handlers/employeeHanders";
+import { createTransport } from 'nodemailer';
 
 export const employeeRouter = Router();
 
-employeeRouter.get('/', async (_req, res: express.Response) => {
+employeeRouter.get("/", async (_req, res: express.Response) => {
 	const employees = await employeeHandlers.getAllEmployees();
 	res.json(employees);
 });
+
+employeeRouter.get(
+	"/sendmail/:id",
+	async (req: express.Request, res: express.Response) => {
+		const { id } = req.params;
+		const employees: any = await employeeHandlers.getAllEmployees();
+		const employee = employees.find((m: any) => (String(m.employeeID) === id));
+		const sendTo = "edwardtanguay@gmail.com";
+
+		const mailOptions = {
+			from: `Language Community Site <${process.env.GOOGLE_MAIL_ACCOUNT_USER}@gmail.com>`,
+			to: sendTo,
+			subject: `Information on employee #${employee.employeeID}`,
+			html: `
+    Employee #${employee.employeeID}: ${employee.firstName} ${employee.lastName} - ${employee.title}
+    `,
+		};
+
+		const transporter = createTransport({
+			service: "gmail",
+			auth: {
+				user: process.env.GOOGLE_MAIL_ACCOUNT_USER,
+				pass: process.env.GOOGLE_MAIL_NODEMAILER_PASSWORD,
+			},
+		});
+
+		transporter.sendMail(mailOptions, (error: any, info: any) => {
+			if (error) {
+				res.send(`ERROR: ${error.message}`);
+			} else {
+				res.send("Email sent: " + info.response);
+			}
+		});
+	}
+);
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import express from 'express';
-import cors from 'cors';
-import { employeeRouter } from './routers/employeeRouter';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import { employeeRouter } from "./routers/employeeRouter";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,18 +10,18 @@ export const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
 	res.send(`
 <html>
 	<body>
 	<h1>${process.env.APP_TITLE}</h1>
-	<ul>
-		<li>GET all employees - <a href="/employees">/employees</a></li>
-	</ul>
+	    <ul>
+        <li>GET - show all employees - <a href="/employees">/employees</a></li>
+        <li>GET - send mail about employee 3 <a href="/employees/sendmail/3">/employees/sendmail/3</a></li>
+    </ul>
 	</body>
 </html>
-	`)
-
+	`);
 });
 
-app.use('/employees', employeeRouter);
+app.use("/employees", employeeRouter);
