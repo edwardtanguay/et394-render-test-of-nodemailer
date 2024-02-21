fda65fd4a04878486a2780b3baceb68fe5e05375
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-21 13:27:23 +0100

    finished ready for publish

--- src/routers/employeeRouter.ts
+++ src/routers/employeeRouter.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Router } from "express";
 import * as employeeHandlers from "../handlers/employeeHanders";
-import { createTransport } from 'nodemailer';
+import { createTransport } from "nodemailer";
 
 export const employeeRouter = Router();
 
@@ -10,37 +10,45 @@ employeeRouter.get("/", async (_req, res: express.Response) => {
 	res.json(employees);
 });
 
-employeeRouter.get(
+employeeRouter.post(
 	"/sendmail/:id",
 	async (req: express.Request, res: express.Response) => {
 		const { id } = req.params;
-		const employees: any = await employeeHandlers.getAllEmployees();
-		const employee = employees.find((m: any) => (String(m.employeeID) === id));
-		const sendTo = "edwardtanguay@gmail.com";
+		const { pin } = req.body;
 
-		const mailOptions = {
-			from: `Language Community Site <${process.env.GOOGLE_MAIL_ACCOUNT_USER}@gmail.com>`,
-			to: sendTo,
-			subject: `Information on employee #${employee.employeeID}`,
-			html: `
-    Employee #${employee.employeeID}: ${employee.firstName} ${employee.lastName} - ${employee.title}
-    `,
-		};
+		if (pin !== process.env.PIN) {
+			res.status(401).send("not authorized");
+		} else {
+			const employees: any = await employeeHandlers.getAllEmployees();
+			const employee = employees.find(
+				(m: any) => String(m.employeeID) === id
+			);
+			const sendTo = "edwardtanguay@gmail.com";
 
-		const transporter = createTransport({
-			service: "gmail",
-			auth: {
-				user: process.env.GOOGLE_MAIL_ACCOUNT_USER,
-				pass: process.env.GOOGLE_MAIL_NODEMAILER_PASSWORD,
-			},
-		});
+			const mailOptions = {
+				from: `Language Community Site <${process.env.GOOGLE_MAIL_ACCOUNT_USER}@gmail.com>`,
+				to: sendTo,
+				subject: `Information on employee #${employee.employeeID}`,
+				html: `
+		Employee #${employee.employeeID}: ${employee.firstName} ${employee.lastName} - ${employee.title}
+		`,
+			};
 
-		transporter.sendMail(mailOptions, (error: any, info: any) => {
-			if (error) {
-				res.send(`ERROR: ${error.message}`);
-			} else {
-				res.send("Email sent: " + info.response);
-			}
-		});
+			const transporter = createTransport({
+				service: "gmail",
+				auth: {
+					user: process.env.GOOGLE_MAIL_ACCOUNT_USER,
+					pass: process.env.GOOGLE_MAIL_NODEMAILER_PASSWORD,
+				},
+			});
+
+			transporter.sendMail(mailOptions, (error: any, info: any) => {
+				if (error) {
+					res.send(`ERROR: ${error.message}`);
+				} else {
+					res.send("Email sent: " + info.response);
+				}
+			});
+		}
 	}
 );
